2115a389045c4100b66a3233bd938d967c650b9e
RyanCrocombe <ryancrocombe@gmail.com>
2016-11-18 11:35:04 +0000

    Added article CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root "pages#home"
+
   get 'concepts/index'
 
   get 'posts/index'
 
-  root "pages#home"
+  resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
