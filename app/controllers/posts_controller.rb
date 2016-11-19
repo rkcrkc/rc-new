39b551028f9d35b9e00eab2307740ee7db8d418e
RyanCrocombe <ryancrocombe@gmail.com>
2016-11-19 21:46:26 +0000

    Minor tweaks before launch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 2)
+    @posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 8)
   end
 
   def new
